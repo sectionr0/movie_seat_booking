ce9697c6d5bbab7f483bd921ba134a20c5ca9f96
H <sectionr0@gmail.com>
2021-02-04 07:19:39 +0900

    update UI(count,price)

--- Seatbooking.js
+++ Seatbooking.js
@@ -1,7 +1,22 @@
 const container = document.querySelector('.seat-container');
+const movieselect = document.getElementById('movielist');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const moivecount = document.getElementById('count');
+const totalcount = document.getElementById('total');
 
-function updateUI() {
+function saveMoiveData(count, total) {
+    localStorage.setItem('selectedSeatsCount', count);
+    localStorage.setItem('selectedMovieIndex', movieselect.value);
+    localStorage.setItem('totalcount', total);
+}
+
+function updateCount() {
+    const getmoivecount = localStorage.getItem('selectedSeatsCount');
+    const getmoivetotal = localStorage.getItem('totalcount');
+    moivecount.innerText = getmoivecount;
+    totalcount.innerText = getmoivetotal;
+}
+function updateSeat() {
     const updateseat = JSON.parse(localStorage.getItem('selectedSeats'));
 
     if (updateseat !== null && updateseat.length > 0) {
@@ -12,16 +27,33 @@ function updateUI() {
             }
         })
     }
+}
 
+function updateUI() {
+    updateSeat();
+    updateCount();
 }
 
 function updateSelectCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
     //console.log(selectedSeats);
     const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
-    //console.log(seatsIndex);
 
     localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+
+
+    //console.log(movieselect.selectedIndex);
+
+    const selectedSeatsCount = seatsIndex.length;
+
+    moivecount.innerText = selectedSeatsCount;
+    totalcount.innerText = selectedSeatsCount * movieselect.value;
+
+    saveMoiveData(selectedSeatsCount, selectedSeatsCount * movieselect.value);
+
+
+
 }
 
 container.addEventListener('click', event => {
@@ -37,6 +69,13 @@ container.addEventListener('click', event => {
 
 
 
+})
+
+movieselect.addEventListener('change', event => {
+    const selectedSeatsCount = localStorage.getItem('selectedSeatsCount');
+    //console.log(selectedSeatsCount, event.target.selectedIndex);
+    saveMoiveData(selectedSeatsCount, event.target.value * selectedSeatsCount);
+    updateCount();
 })
 
 function init() {
